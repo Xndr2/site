414c2ddb728656659f08367f7eab3babf8fcc4ac
Xndr2 <moermanxander@gmail.com>
2023-12-14 00:10:19 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       />
       <main className="max-w-screen-xl text-center mx-auto h-full animate-title">
         <div className="text-xl pt-20 md:pt-80 px-4">
-          <h1 className="text-3xl mb-4">Hi, I'm <p className="text-blue-500 inline">Xander</p>.</h1>
+          <h1 className="text-3xl mb-4">Hi, I&apos;m <p className="text-blue-500 inline">Xander</p>.</h1>
           <h2>A self taught developer from <p className="text-blue-500 inline">Belgium</p>. I create applications or systems for people online.</h2>
           <h3 className="text-zinc-500 text-base">
             Currently working on{" "}
