5ee26de2f6a0362768f88cc670269a90cb6fde06
Xndr2 <moermanxander@gmail.com>
2024-01-22 16:16:04 +0100

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 import { Montserrat } from 'next/font/google'
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={montserrat.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
