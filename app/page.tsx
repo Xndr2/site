c598a31a34bca047c8972e5db1f999caedf8b7f5
Xndr <moermanxander@gmail.com>
2024-05-29 19:26:55 +0200

    main page email

--- app/page.tsx
+++ app/page.tsx
@@ -26,12 +26,16 @@ export default function Home() {
       <main className="max-w-screen-xl text-center mx-auto h-full animate-title">
         <div className="text-xl pt-20 md:pt-80 px-4">
           <h1 className="text-3xl mb-4">Hi, I&apos;m <p className="text-blue-500 inline">Xander</p>.</h1>
-          <h2>A self taught developer from <p className="text-blue-500 inline">Belgium</p>. I create applications or systems for people online.</h2>
-          <h3 className="text-zinc-500 text-base">
+          <h2>
+            A self taught developer from <p className="text-blue-500 inline">Belgium</p>.
+            <br />
             Project Lead @ {" "}
-            <a href="https://www.headshotinteractive.com/" className="underline duration-500 hover:text-zinc-300">
+            <a href="https://www.headshotinteractive.com/" className="underline underline-offset-4 duration-500 hover:text-zinc-300">
               Headshot Interactive
-            </a> working on <Link href="abandoned" className="underline duration-500 hover:text-zinc-300">Abandoned</Link>
+            </a> working on <Link href="abandoned" className="underline underline-offset-4 duration-500 hover:text-zinc-300">Abandoned</Link>
+          </h2>
+          <h3 className="text-zinc-400 text-base">
+            For any business related stuff you can quickly reach me by emailing to <Link className="underline underline-offset-4 text-blue-500" href="mailto:moermanxander@onmail.com">business@xndr.site</Link>
           </h3>
         </div>
       </main>
